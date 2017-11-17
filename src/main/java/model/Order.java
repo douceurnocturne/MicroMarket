8c934edd6a4cf65df4d1065236992ee54c8633d9
Ehsan <ehsanelwan@icoud.com>
2017-11-17 17:20:36 +0100

    Implementing Order model

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -5,20 +5,40 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author Ehsan
  */
 public class Order {
 
-    private final Customer ct;
-    private final int ordernum;
+    private final Customer customer;
+    private final int ordernumber;
+    private final int quantity;
     private final Product produit;
-    
-    public Order(int ordernumber, Customer customer, Product prod) {
-        this.ct = customer;
-        this.ordernum = ordernumber;
-        this.produit = prod;
-    }
+    private final float shippingcost;
+    private final Date date;
 
+    public Order(int ordernumber, Customer customer, Product produit, int quantity, float shippingcost, Date date) {
+        this.customer = customer;
+        this.ordernumber = ordernumber;
+        this.produit = produit;
+        this.quantity = quantity;
+        this.shippingcost = shippingcost;
+        this.date = date;
+
+    }
+    
+    public Date GetDate(){
+        return this.date;
+    }
+    
+    public float GetOrderCostWithShipping(){
+        return (this.quantity*this.produit.GetPrice())+this.shippingcost;
+    }
+    
+    public int GetNumber(){
+        return this.ordernumber;
+    }
 }
