6f024389d39ccf9fb26ea4023c83a4c6cb6ee53b
xzait <xzait@LAPTOP-OA60C6GS>
2017-11-21 01:11:06 +0100

    Test ; OrderByCustomer : problem

--- src/test/java/model/DAOTest.java
+++ src/test/java/model/DAOTest.java
@@ -6,16 +6,11 @@
 package model;
 
 import java.sql.Date;
+import java.util.List;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import javax.sql.DataSource;
-import org.junit.*;
-import static org.junit.Assert.*;
-import model.Order;
 
 /**
  *
@@ -54,8 +49,20 @@ public class DAOTest {
     }
     
     @Test
+    public void testOrderByCustomer(){
+        //Le client d'id = 3
+        Customer client3 = new Customer(3, "Small Bill Company", "8585 South Upper Murray Drive", "GA", "Alanta", "www.smallbill.example.com");
+        //Son produit commander
+        Product produit3 = new Product(980030, (float) 59.95, 250, true, "10Gb Ram", "Poney Express");
+        //La seul commande de ce client
+        Date date3 = new Date(24, 05, 2011);
+        Order commande3 = new Order(10398004, client3, produit3, 10, (float) 275.00, date3, "Poney Express");
+        DAO dao = new DAO(DataSourceFactory.getDataSource(DataSourceFactory.DriverType.embedded));
+        List<Order> L = dao.GetOrderByCustomer(client3);
+        System.out.println(L);
+        assertEquals(L.get(0),commande3);
+    }
     
-   
     @After
     public void tearDown() {
     }
